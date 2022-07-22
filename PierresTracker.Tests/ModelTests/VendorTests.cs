ef5b5337864d19e8eaea9bd2a16edf0d795a3821
Donovan Weber <donovanweber03@gmail.com>
2022-07-22 13:38:27 -0400

    Pass 4th test and add GetAll()

--- PierresTracker.Tests/ModelTests/VendorTests.cs
+++ PierresTracker.Tests/ModelTests/VendorTests.cs
@@ -36,5 +36,18 @@ namespace Tracker.Tests
       int result = newVendor.Id;
       Assert.AreEqual(1, result);
     }
+    [TestMethod]
+    public void GetAll_ReturnsAllVendorObjects_VendorList()
+    {
+      string vendorName1 = "Test Cafe";
+      string vendorName2 = "Cafe de Test";
+      Vendor newVendor1 = new Vendor(vendorName1);
+      Vendor newVendor2 = new Vendor(vendorName2);
+
+      List<Vendor> newList = new List<Vendor> { newVendor1, newVendor2};
+
+      List<Vendor> result = Vendor.GetAll();
+      CollectionAssert.AreEqual(newList, result);
+    }
   }
 }  

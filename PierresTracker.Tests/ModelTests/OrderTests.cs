dfa2c9ade1e4ae422245a9804f6c204b9f969594
Donovan Weber <donovanweber03@gmail.com>
2022-07-22 12:48:13 -0400

    Red: needs method to create instance of Vendor

---
+++ PierresTracker.Tests/ModelTests/OrderTests.cs
@@ -0,0 +1,20 @@
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tracker.Models;
+using System.Collections.Generic;
+using System;
+
+namespace Tracker.Tests
+{
+  [TestClass]
+  public class OrderTests //: IDisposable
+  {
+
+    // public void Dispose()
+    // {
+    //   Category.ClearAll();
+    // }
+  }
+
+  
+}  

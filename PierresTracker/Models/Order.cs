9a60eab537984f1686719e9d95437e0cabd65f42
Donovan Weber <donovanweber03@gmail.com>
2022-07-22 13:56:05 -0400

    Pass 6th test and add an order object to vendor object

--- PierresTracker/Models/Order.cs
+++ PierresTracker/Models/Order.cs
@@ -5,6 +5,11 @@ namespace Tracker.Models
 {
   public class Order
   {
-    //
+    public string Description { get; set; }
+    
+    public Order(string description)
+    {
+      Description = description;
+    }
   }
 }
